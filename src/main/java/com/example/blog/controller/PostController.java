8bd440a8f72d17e23a16faa6d7184bd8d05a8db5
brenomp730@gmail.com <brenomp730@gmail.com>
2025-10-29 21:30:07 -0300

    feat: PostController added

---
+++ src/main/java/com/example/blog/controller/PostController.java
@@ -0,0 +1,15 @@
+package com.example.blog.controller;
+
+import com.example.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+    private PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+}
