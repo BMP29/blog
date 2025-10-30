9c4e93abba09a3e65053f8bdd5b4fb950f8690b2
brenomp730@gmail.com <brenomp730@gmail.com>
2025-10-29 22:51:16 -0300

    feat: createNewPost method added to PostService

--- src/main/java/com/example/blog/service/PostService.java
+++ src/main/java/com/example/blog/service/PostService.java
@@ -1,5 +1,7 @@
 package com.example.blog.service;
 
+import com.example.blog.DTOs.PostDTO;
+import com.example.blog.model.Post;
 import com.example.blog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +13,25 @@ public class PostService {
     @Autowired
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
+    }
+
+    public PostDTO createNewPost(PostDTO postDTO) {
+        Post newPost = new Post(
+                postDTO.title(),
+                postDTO.author(),
+                postDTO.content(),
+                postDTO.createdAt(),
+                postDTO.updatedAt()
+        );
+
+        this.postRepository.save(newPost);
+
+        return new PostDTO(
+                newPost.getTitle(),
+                newPost.getAuthor(),
+                newPost.getAuthor(),
+                newPost.getCreatedAt(),
+                newPost.getUpdatedAt()
+        );
     }
 }
