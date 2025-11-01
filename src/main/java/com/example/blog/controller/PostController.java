b957a7425e18888fe9f2d7339fc91509a3a33047
brenomp730@gmail.com <brenomp730@gmail.com>
2025-10-31 22:11:36 -0300

    feat: endPoint for creating Post added

--- src/main/java/com/example/blog/controller/PostController.java
+++ src/main/java/com/example/blog/controller/PostController.java
@@ -1,7 +1,12 @@
 package com.example.blog.controller;
 
+import com.example.blog.DTOs.PostDTO;
 import com.example.blog.service.PostService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,12 @@ public class PostController {
     @Autowired
     public PostController(PostService postService) {
         this.postService = postService;
+    }
+
+    @PostMapping
+    public ResponseEntity createNewPost(@Valid @RequestBody PostDTO postDTO) {
+        PostDTO post = this.postService.createNewPost(postDTO);
+
+        return ResponseEntity.ok(post);
     }
 }
