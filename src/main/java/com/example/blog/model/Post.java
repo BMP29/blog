111e9ff3a581a6a37bf790cd2f1cbb1eb1310225
brenomp730@gmail.com <brenomp730@gmail.com>
2025-10-31 22:10:17 -0300

    fix: @Id import fixed

--- src/main/java/com/example/blog/model/Post.java
+++ src/main/java/com/example/blog/model/Post.java
@@ -3,7 +3,6 @@ package com.example.blog.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.data.annotation.Id;
 
 import java.time.LocalDate;
 
