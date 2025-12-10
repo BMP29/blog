6fcdec40dc931c7c6a4361c8e542c4847d019d63
brenomp730@gmail.com <brenomp730@gmail.com>
2025-12-09 22:55:23 -0300

    feat: getPosts method added

--- src/main/java/com/example/blog/service/PostService.java
+++ src/main/java/com/example/blog/service/PostService.java
@@ -1,10 +1,17 @@
 package com.example.blog.service;
 
+import com.example.blog.DTOs.PageResponse;
 import com.example.blog.DTOs.PostDTO;
+import com.example.blog.DTOs.PostSummaryDTO;
 import com.example.blog.model.Post;
 import com.example.blog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PagedModel;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
@@ -32,6 +39,24 @@ public class PostService {
                 newPost.getAuthor(),
                 newPost.getCreatedAt(),
                 newPost.getUpdatedAt()
+        );
+    }
+
+    public PageResponse<PagedModel<PostSummaryDTO>> getPosts(String nextCursor, int limit) {
+        Page<PostSummaryDTO> postSlice = postRepository.findAfter(nextCursor, PageRequest.of(0, limit));
+
+        if (!postSlice.hasContent()) {
+            return new PageResponse<>(null, null, null);
+        }
+
+        List<PostSummaryDTO> posts = postSlice.getContent();
+
+        PagedModel<PostSummaryDTO> page = new PagedModel<>(postSlice);
+
+        return new PageResponse<>(
+                page,
+                posts.get(0).id().toString(),
+                posts.get(posts.size() - 1).id().toString()
         );
     }
 }
