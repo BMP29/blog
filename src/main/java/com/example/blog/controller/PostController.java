780c8e8127ecf02ad98b59c0e8fd67df75292844
brenomp730@gmail.com <brenomp730@gmail.com>
2025-12-09 22:57:50 -0300

    feat: getPosts endpoint added

--- src/main/java/com/example/blog/controller/PostController.java
+++ src/main/java/com/example/blog/controller/PostController.java
@@ -1,14 +1,15 @@
 package com.example.blog.controller;
 
+import com.example.blog.DTOs.PageResponse;
 import com.example.blog.DTOs.PostDTO;
+import com.example.blog.DTOs.PostSummaryDTO;
 import com.example.blog.service.PostService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/post")
@@ -21,9 +22,19 @@ public class PostController {
     }
 
     @PostMapping
-    public ResponseEntity createNewPost(@Valid @RequestBody PostDTO postDTO) {
+    public ResponseEntity<PostDTO> createNewPost(@Valid @RequestBody PostDTO postDTO) {
         PostDTO post = this.postService.createNewPost(postDTO);
 
         return ResponseEntity.ok(post);
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<PagedModel<PostSummaryDTO>>> getPosts(
+            @RequestParam(defaultValue = "5", required = false) int size,
+            @RequestParam(defaultValue = "9999999999", required = false) String nextCursor
+    ) {
+        PageResponse<PagedModel<PostSummaryDTO>> postPage = postService.getPosts(nextCursor, size);
+
+        return ResponseEntity.ok(postPage);
     }
 }
