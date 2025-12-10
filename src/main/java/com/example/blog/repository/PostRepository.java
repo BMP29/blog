aa65cf03df56f486f173d7764ffb3c22d39a80d9
brenomp730@gmail.com <brenomp730@gmail.com>
2025-12-09 22:54:39 -0300

    feat: findAfter method added

--- src/main/java/com/example/blog/repository/PostRepository.java
+++ src/main/java/com/example/blog/repository/PostRepository.java
@@ -1,9 +1,19 @@
 package com.example.blog.repository;
 
+import com.example.blog.DTOs.PostSummaryDTO;
 import com.example.blog.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
+
+    @Query("select p.id, p.title, p.author, p.createdAt, p.updatedAt from Post p where p.id < :cursorValue order by p.id desc")
+    Page<PostSummaryDTO> findAfter(@Param("cursorValue") String cursorValue, Pageable pageable);
+
 }
