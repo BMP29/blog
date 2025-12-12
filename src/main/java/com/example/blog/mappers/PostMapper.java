53ed787199f421494ac1d7125daef230b35c8b38
brenomp730@gmail.com <brenomp730@gmail.com>
2025-12-11 21:07:03 -0300

    feat: mapper implemented

--- src/main/java/com/example/blog/mappers/PostMapper.java
+++ src/main/java/com/example/blog/mappers/PostMapper.java
@@ -1,4 +1,18 @@
 package com.example.blog.mappers;
 
+import com.example.blog.DTOs.PostDTO;
+import com.example.blog.model.Post;
+
 public class PostMapper {
+    public static PostDTO toDTO(Post post) {
+        if(post == null) return null;
+
+        return new PostDTO(
+                post.getTitle(),
+                post.getAuthor(),
+                post.getContent(),
+                post.getCreatedAt(),
+                post.getUpdatedAt()
+        );
+    }
 }
