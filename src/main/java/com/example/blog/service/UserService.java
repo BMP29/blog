4788cf19a458271bdbab570440dd293dac66a228
brenomp730@gmail.com <brenomp730@gmail.com>
2026-01-05 15:58:18 -0300

    feat: userService implemented, no methods added

---
+++ src/main/java/com/example/blog/service/UserService.java
@@ -0,0 +1,16 @@
+package com.example.blog.service;
+
+import com.example.blog.model.User;
+import com.example.blog.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository, EmailService emailService) {
+        this.userRepository = userRepository;
+    }
+}
