17722cec6d4098e24a9bd1c52ff78de0be3f6026
brenomp730@gmail.com <brenomp730@gmail.com>
2026-01-05 15:53:26 -0300

    feat: verifiationCode, verificationCodeExpirestAt and Enabled fields added; setters now are added by lombok

---
+++ src/main/java/com/example/blog/model/User.java
@@ -0,0 +1,80 @@
+package com.example.blog.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@Getter
+@Setter
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "userSeq"
+    )
+    @SequenceGenerator(
+            name = "postSeq",
+            sequenceName = "postSeq",
+            allocationSize = 1
+    )
+    private long id;
+    @NotBlank
+    @Column(name = "username")
+    private String username;
+    @NotBlank
+    @Column(name = "email")
+    private String email;
+    @NotBlank
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "verification_code")
+    private String verificationCode;
+    @Column(name = "verification_expiration")
+    private LocalDateTime verificationCodeExpiresAt;
+    private boolean enabled;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
