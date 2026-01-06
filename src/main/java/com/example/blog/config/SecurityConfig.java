291004aab3cacfa60cac00ea21f79f963d2cd754
brenomp730@gmail.com <brenomp730@gmail.com>
2026-01-06 15:53:06 -0300

    Feat: add GET /posts endpoints are public now
    GET requests to /posts don't need to authenticate anymore

--- src/main/java/com/example/blog/config/SecurityConfig.java
+++ src/main/java/com/example/blog/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,6 +35,7 @@ public class SecurityConfig {
                 .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/posts/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
