d173296f96440ee7dce00fef0a4b7646b5550bff
brenomp730@gmail.com <brenomp730@gmail.com>
2026-01-08 15:44:38 -0300

    Feat: add Global Exeception handler

---
+++ src/main/java/com/example/blog/controller/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.blog.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleException(ResponseStatusException ex) {
+        return ResponseEntity.status(ex.getStatusCode())
+                .body(ex.getReason());
+    }
+}
