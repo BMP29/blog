5430252c3db51b467253b85dac07454b7c955d5c
brenomp730@gmail.com <brenomp730@gmail.com>
2026-01-08 17:38:18 -0300

    Style: removing unused imports

--- src/main/java/com/example/blog/service/UserService.java
+++ src/main/java/com/example/blog/service/UserService.java
@@ -1,11 +1,7 @@
 package com.example.blog.service;
 
-import com.example.blog.model.User;
 import com.example.blog.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class UserService {
