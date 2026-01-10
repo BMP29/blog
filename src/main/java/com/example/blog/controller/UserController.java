3e032e422c664911e133f14dea97a735a0c7ffc6
brenomp730@gmail.com <brenomp730@gmail.com>
2026-01-10 14:01:43 -0300

    Feat: add /username to change username

--- src/main/java/com/example/blog/controller/UserController.java
+++ src/main/java/com/example/blog/controller/UserController.java
@@ -2,12 +2,11 @@ package com.example.blog.controller;
 
 import com.example.blog.model.User;
 import com.example.blog.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/users")
 @RestController
@@ -22,5 +21,12 @@ public class UserController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User currentUser = (User) authentication.getPrincipal();
         return ResponseEntity.ok(currentUser);
+    }
+
+    @PatchMapping("/username/{username}")
+    public ResponseEntity<Void> changeUsername(@PathVariable String username) {
+        this.userService.changeUsername(username);
+
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 }
