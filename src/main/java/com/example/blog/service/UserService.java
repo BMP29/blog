9d6c5effd62c953380c601c2970fe85604dd9195
brenomp730@gmail.com <brenomp730@gmail.com>
2026-01-10 14:01:12 -0300

    Feat: add changeUsername method

--- src/main/java/com/example/blog/service/UserService.java
+++ src/main/java/com/example/blog/service/UserService.java
@@ -1,6 +1,9 @@
 package com.example.blog.service;
 
+import com.example.blog.model.User;
 import com.example.blog.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,5 +11,14 @@ public class UserService {
     private final UserRepository userRepository;
     public UserService(UserRepository userRepository, EmailService emailService) {
         this.userRepository = userRepository;
+    }
+
+    public void changeUsername(String newUsername) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+
+        user.setUsername(newUsername);
+
+        this.userRepository.save(user);
     }
 }
